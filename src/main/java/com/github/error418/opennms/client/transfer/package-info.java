019d1c4580ef3324dd86b2ea108bc72e7c77cb81
error418 <gerbigmic+github@googlemail.com>
2016-06-19 20:59:56 +0200

    add gh-pages content

--- src/main/java/com/github/error418/opennms/client/transfer/package-info.java
+++
@@ -1,4 +0,0 @@
-@XmlSchema(namespace = "http://xmlns.opennms.org/xsd/event", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.github.error418.opennms.client.transfer;
-
-import javax.xml.bind.annotation.XmlSchema;
