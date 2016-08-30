763007cf6acb4dae8433ddab168b2c8e7c41c4e0
Michael Gerbig <error418@users.noreply.github.com>
2016-08-30 12:47:18 +0200

    Update Severity.java
    change xml identifiers to string representation

--- src/main/java/com/github/error418/opennms/client/transfer/Severity.java
+++ src/main/java/com/github/error418/opennms/client/transfer/Severity.java
@@ -16,31 +16,31 @@ public enum Severity {
 	 * event. Everyone who can should stop what they are doing and focus on
 	 * fixing the problem.
 	 */
-	@XmlEnumValue("7") CRITICAL(7),
+	@XmlEnumValue("Critical") CRITICAL(7),
 
 	/**
 	 * A device is completely down or in danger of going down. Attention needs
 	 * to be paid to this problem immediately.
 	 */
-	@XmlEnumValue("6") MAJOR(6),
+	@XmlEnumValue("Major") MAJOR(6),
 
 	/**
 	 * A part of a device (a service, and interface, a power supply, etc.) has
 	 * stopped functioning. The device needs attention.
 	 */
-	@XmlEnumValue("5") MINOR(5),
+	@XmlEnumValue("Minor") MINOR(5),
 
 	/**
 	 * An event has occurred that may require action. This severity can also be
 	 * used to indicate a condition that should be noted (logged) but does not
 	 * require direct action.
 	 */
-	@XmlEnumValue("4") WARNING(4),
+	@XmlEnumValue("Warning") WARNING(4),
 
 	/**
 	 * Informational message. No action required.
 	 */
-	@XmlEnumValue("3") NORMAL(3),
+	@XmlEnumValue("Normal") NORMAL(3),
 
 	/**
 	 * This severity is reserved for use in Alarms to indicate that an alarm
@@ -49,12 +49,12 @@ public enum Severity {
 	 * 
 	 * Please use "Normal" severity for events that clear an alarm.
 	 */
-	@XmlEnumValue("2") CLEARED(2),
+	@XmlEnumValue("Cleared") CLEARED(2),
 
 	/**
 	 * No Severity could be associated with this event.
 	 */
-	@XmlEnumValue("1") INDETERMINATE(1);
+	@XmlEnumValue("Indeterminate") INDETERMINATE(1);
 
 	private int severityValue;
 
